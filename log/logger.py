f67b47968451533dd40cfd1015a17169bc37178c
Yihan Ling <carson.ling1102@outlook.com>
2025-06-16 14:19:11 -0400

    hidden logging in console

--- log/logger.py
+++ log/logger.py
@@ -9,14 +9,14 @@ file_handler = logging.FileHandler("./process_manager/log/process_manager.log")
 file_handler.setLevel(logging.DEBUG)
 
 # Create a console handler to log to the console
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+# console_handler = logging.StreamHandler()
+# console_handler.setLevel(logging.INFO)
 
 # Create a formatter and set it for both handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
+# console_handler.setFormatter(formatter)
 
 # Add the handlers to the logger
 logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+# logger.addHandler(console_handler)
