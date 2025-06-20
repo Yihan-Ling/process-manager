a5ed7fb3cf575b866692a70451b0addc749ac713
Yihan Ling <carson.ling1102@outlook.com>
2025-06-20 15:31:35 -0400

    started switching to logging

--- dummy_processes/d_one.py
+++ dummy_processes/d_one.py
@@ -1,9 +1,11 @@
 from time import sleep
+from process_manager.log import logger
 
+_log = logger(__file__)
 var = 0
 while  var<15:
-    print(var)
+    _log.info(var)
     var += 1
     sleep(1)
-print("d_one exits")
+_log.warning("d_one exits")
         
--- log/logger.py
+++ log/logger.py
@@ -1,22 +1,33 @@
+# import logging
+
+# # Set up a custom logger
+# logger = logging.getLogger("process_manager")
+# logger.setLevel(logging.DEBUG)
+
+# # Create a file handler to log to a file
+# file_handler = logging.FileHandler("./process_manager/log/process_manager.log")
+# file_handler.setLevel(logging.DEBUG)
+
+# # Create a console handler to log to the console
+# # console_handler = logging.StreamHandler()
+# # console_handler.setLevel(logging.INFO)
+
+# # Create a formatter and set it for both handlers
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# file_handler.setFormatter(formatter)
+# # console_handler.setFormatter(formatter)
+
+# # Add the handlers to the logger
+# logger.addHandler(file_handler)
+# # logger.addHandler(console_handler)
+
+from pathlib import Path
 import logging
 
-# Set up a custom logger
-logger = logging.getLogger("process_manager")
-logger.setLevel(logging.DEBUG)
+def logger_name(target: str) -> str:
+    rpath = Path(target).resolve().relative_to(Path(__file__).parent.parent)
+    parts = list(rpath.parts[:-1]) + [rpath.stem]
+    return '.'.join(parts)
 
-# Create a file handler to log to a file
-file_handler = logging.FileHandler("./process_manager/log/process_manager.log")
-file_handler.setLevel(logging.DEBUG)
-
-# Create a console handler to log to the console
-# console_handler = logging.StreamHandler()
-# console_handler.setLevel(logging.INFO)
-
-# Create a formatter and set it for both handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-# console_handler.setFormatter(formatter)
-
-# Add the handlers to the logger
-logger.addHandler(file_handler)
-# logger.addHandler(console_handler)
+def logger(target: Path) -> logging.Logger:
+    return logging.getLogger(logger_name(target))
