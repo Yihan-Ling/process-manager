2348fbe2a9efd304f9066da98cdcd0d39cb91532
Yihan Ling <carson.ling1102@outlook.com>
2025-07-15 15:49:16 -0400

    added helper functions for processes to call

--- src/process_manager/util/__init__.py
+++ src/process_manager/util/__init__.py
@@ -1,3 +1,18 @@
+import igmr_robotics_toolkit.comms.auto_init    # Has to import before CycloneDDS
+from cyclonedds.pub import Publisher, DataWriter
+from cyclonedds.topic import Topic
+
+from cyclonedds.sub import Subscriber, DataReader
+from cyclonedds.qos import Qos, Policy
+from cyclonedds.pub import Publisher, DataWriter
+from cyclonedds.topic import Topic
+
+from igmr_robotics_toolkit.comms.params import ParameterClient, StateClient
+
+from process_manager.types import Heartbeat
+import time
+from logging import Logger
+
 def default_logging():
     import logging, coloredlogs
     coloredlogs.DEFAULT_LOG_FORMAT = '%(asctime)s.%(msecs)03d %(filename)15s:%(lineno)03d %(levelname)7s: %(message)s'
@@ -5,3 +20,26 @@ def default_logging():
     coloredlogs.install(level=logging.DEBUG, logger=logging.getLogger('ui'))
     coloredlogs.install(level=logging.DEBUG, logger=logging.getLogger('dummy_processes'))
     coloredlogs.install(level=logging.DEBUG, logger=logging.getLogger('node'))
+
+def write_heartbeat(writer: DataWriter, module: str = None):
+    """Send a heartbeat using a DDS writer and a resolved module name from parameters."""
+    params = ParameterClient()
+    with params:
+        resolved_name = params.get(f"processes/{module}")
+
+    writer.write(Heartbeat(name=resolved_name, timestamp=time.time()))
+    
+def create_writer(_log:Logger) -> DataWriter:
+    try:
+        _log.warning("loading parameters")
+        params = ParameterClient()
+        sc = StateClient()
+    except ParameterClient.InitializationTimeout:
+        _log.error('parameter initialization timed out (is the parameter server running?)')
+        raise SystemExit(1)
+
+    with params:
+        dp = params.participant
+        pub = Publisher(dp)
+        state_writer = DataWriter(pub, Topic(dp, "heart_beats", Heartbeat))
+    return state_writer 
